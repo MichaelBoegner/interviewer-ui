84cf5ce2999c26aab785871e87b016f89d3bd094
michaelboegner <cmikeb@gmail.com>
2025-03-10 11:00:57 +0700

    fixed ASCII title

--- interviewer-ui/src/App.jsx
+++ interviewer-ui/src/App.jsx
@@ -6,16 +6,29 @@ import './App.css';
 // Add this ASCII art banner function for the retro vibe
 const AsciiHeader = ({ text }) => {
   return (
-    <pre className="text-green-500 font-mono text-xs md:text-sm leading-tight overflow-hidden my-2">
-{`
- ___ _  _ _____ ___ ___ _   _ ___ _____      _____ ___ 
-|_ _| \\| |_   _| __| _ \\ | | |_ _| __\\\\ \\    / / __| _ \\
- | || .\` | | | | _||   / |_| || || _| \\ \\/\\/ /| _||   /
-|___|_|\\_| |_| |___|_|_\\\\___/|___|___| \\_/\\_/ |___|_|_\\
-                                                     
+    <div className="my-2">
+      <pre className="text-green-500 ascii-header" style={{
+        fontFamily: "'Fira Code', 'Courier New', monospace",
+        fontSize: "0.7rem",
+        lineHeight: 1.2,
+        whiteSpace: "pre",
+        letterSpacing: 0,
+        textAlign: "center",
+        margin: 0,
+        padding: 0,
+        width: "100%",
+        fontWeight: "bold",
+        fontVariantLigatures: "none",
+      }}>{`
+  ___ _   _ _____ _____ ______     _____ _______        _______ ____  
+ |_ _| \\ | |_   _| ____|  _ \\ \\   / /_ _| ____\\ \\      / / ____|  _ \\ 
+  | ||  \\| | | | |  _| | |_) \\ \\ / / | ||  _|  \\ \\ /\\ / /|  _| | |_) |
+  | || |\\  | | | | |___|  _ < \\ V /  | || |___  \\ V  V / | |___|  _ < 
+ |___|_| \\_| |_| |_____|_| \\_\\ \\_/  |___|_____|  \\_/\\_/  |_____|_| \\_\\
+                                                                       
 ${'>>'} ${text}
-`}
-    </pre>
+`}</pre>
+    </div>
   );
 };
 
