6d278e514a823ac4f5212d4d6c224248cd6674f7
michaelboegner <cmikeb@gmail.com>
2025-03-15 16:07:43 +0700

    small change to test auto deploy

--- interviewer-ui/src/App.jsx
+++ interviewer-ui/src/App.jsx
@@ -6,7 +6,7 @@ import './App.css';
 // Add this constant at the top of the file, after imports
 const API_URL = import.meta.env.VITE_API_URL;
 
-// Add this ASCII art banner function for the retro vibe
+// Add this ASCII art banner function for the retro vibe.
 const AsciiHeader = ({ text }) => {
   return (
     <div className="my-2">
